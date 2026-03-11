1a5f2e95fe1935a96ddbaf39077fce9236b84b2e
Maulik Koli <maulikkoli1021@gmail.com>
2026-03-11 23:24:49 +0530

    init

---
+++ components/hero.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+
+const Hero: React.FC = () => {
+    return (
+        <div className='pb-20 pt-36'>
+            <div className='text-white'>
+                Setup
+            </div>
+        </div>
+    )
+}
+
+export default Hero
